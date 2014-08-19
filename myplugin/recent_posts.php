41ecb32984ced4347471e3e15f7b54942e630176
saumya010 <saumyasharma010@gmail.com>
2014-08-19 11:28:25 +0530

    first commit

---
+++ myplugin/recent_posts.php
@@ -0,0 +1,153 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Recent_Posts extends WP_Widget {
+	// Controller
+	function __construct() {
+	$widget_ops = array('classname' => 'my_widget_class', 'description' => __('Widget that displays details about recent posts', 'wp_widget_plugin'));
+	$control_ops = array('width' => 300, 'height' => 300);
+	parent::WP_Widget(false, $name = __('Recent Posts', 'wp_widget_plugin'), $widget_ops, $control_ops );
+?>
+<?php
+}
+
+function form($instance) { 
+	$defaults = array( 'title' => __('Recent Posts'), 'post_count' => __('5'), 'your_checkbox_var' => __('0')
+            , 'your_checkbox_var1' => __('0'), 'your_checkbox_var2' => __('0'), 'your_checkbox_var3' => __('0')
+            , 'your_checkbox_var4' => __('0'), 'your_checkbox_var5' => __('0'), 'read_more' => __('Read More..'));
+	$instance = wp_parse_args( (array) $instance, $defaults );
+	if ( isset( $instance[ 'title' ] ) ) {
+            $title = $instance[ 'title' ];
+            $post_count=$instance['post_count'];
+            $read_more=$instance['read_more'];
+	}
+	else {
+            $title =$defaults['title'];
+            $post_count=$defaults['post_count'];
+            $read_more=$defaults['read_more'];
+            
+	}?>
+	<p>
+		<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'wp_widget_plugin'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+	</p>
+        <p>
+                <input class="checkbox" type="checkbox" <?php checked($instance['your_checkbox_var'], 'on'); ?> id="<?php echo $this->get_field_id('your_checkbox_var'); ?>" name="<?php echo $this->get_field_name('your_checkbox_var'); ?>" /> 
+                <label for="<?php echo $this->get_field_id('your_checkbox_var'); ?>">Display Featured Image</label>
+        </p>
+        <p>
+                <input class="checkbox" type="checkbox" <?php checked($instance['your_checkbox_var1'], 'on'); ?> id="<?php echo $this->get_field_id('your_checkbox_var1'); ?>" name="<?php echo $this->get_field_name('your_checkbox_var1'); ?>" /> 
+                <label for="<?php echo $this->get_field_id('your_checkbox_var1'); ?>">Display Post Date</label>
+        </p>
+        <p>
+                <input class="checkbox" type="checkbox" <?php checked($instance['your_checkbox_var2'], 'on'); ?> id="<?php echo $this->get_field_id('your_checkbox_var2'); ?>" name="<?php echo $this->get_field_name('your_checkbox_var2'); ?>" /> 
+                <label for="<?php echo $this->get_field_id('your_checkbox_var2'); ?>">Display Post Author</label>
+        </p>
+        <p>
+                <input class="checkbox" type="checkbox" <?php checked($instance['your_checkbox_var3'], 'on'); ?> id="<?php echo $this->get_field_id('your_checkbox_var3'); ?>" name="<?php echo $this->get_field_name('your_checkbox_var3'); ?>" /> 
+                <label for="<?php echo $this->get_field_id('your_checkbox_var3'); ?>">Display Post Category</label>
+        </p>
+        <p>
+                <input class="checkbox" type="checkbox" <?php checked($instance['your_checkbox_var4'], 'on'); ?> id="<?php echo $this->get_field_id('your_checkbox_var4'); ?>" name="<?php echo $this->get_field_name('your_checkbox_var4'); ?>" /> 
+                <label for="<?php echo $this->get_field_id('your_checkbox_var4'); ?>">Display Number of Comments</label>
+        </p>
+        <p>
+                <input class="checkbox" type="checkbox" <?php checked($instance['your_checkbox_var5'], 'on'); ?> id="<?php echo $this->get_field_id('your_checkbox_var5'); ?>" name="<?php echo $this->get_field_name('your_checkbox_var5'); ?>" /> 
+                <label for="<?php echo $this->get_field_id('your_checkbox_var5'); ?>">Display Post Excerpt with Read More link</label>
+        </p>
+        
+	<p>
+		<label for="<?php echo $this->get_field_id('post_count'); ?>"><?php _e('Number of Posts', 'wp_widget_plugin'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('post_count'); ?>" name="<?php echo $this->get_field_name('post_count'); ?>" type="number" value="<?php echo $post_count;?>">
+	</p>
+        
+       
+<?php }
+function update($new_instance,$old_instance){
+    $instance = $old_instance;
+    $instance['title'] = strip_tags( $new_instance['title'] );
+    $instance['post_count'] = strip_tags( $new_instance['post_count'] );
+    $instance['your_checkbox_var'] = $new_instance['your_checkbox_var'];
+    $instance['your_checkbox_var1'] = $new_instance['your_checkbox_var1'];
+    $instance['your_checkbox_var2'] = $new_instance['your_checkbox_var2'];
+    $instance['your_checkbox_var3'] = $new_instance['your_checkbox_var3'];
+    $instance['your_checkbox_var4'] = $new_instance['your_checkbox_var4'];
+    $instance['your_checkbox_var5'] = $new_instance['your_checkbox_var5'];
+    //$instance['read_more'] = strip_tags( $new_instance['read_more'] );
+    
+    return $instance;
+}
+
+function widget($args, $instance) {
+            //if (function_exists("wp_recent_post_popularity_list")) {
+	    $post_count = $instance['post_count'];
+            $title = apply_filters('widget_title', $instance['title']);
+           
+            echo '<div id="recent-posts">';
+            if ( $title )
+                
+                    echo '<h3 class="widget-header text-center">'.$title.'</h3>';
+	//Display the name
+                    $args = new WP_Query(
+                        array(
+                            "posts_per_page" => $post_count,
+                            "post_type" => "post",
+                            "post_status" => "publish",
+                            "meta_key" => "mp_views",
+                            "orderby" => "meta_value_num",
+                            "order" => "DESC"
+                           )
+                    );
+                    global $post;
+                   
+                    if($args->have_posts()) { echo '<ul class="widget-list">'; }
+                    while ( $args->have_posts() ) : $args->the_post();
+                    echo'<li class="widget-list-item">';
+                    echo '<h4><a href="'.get_permalink($post->ID).'">'.the_title('', '', false).'</a></h4>';
+                    if($instance['your_checkbox_var']){
+                        echo "<br>";
+                        display_featured_image();
+                    }
+                    echo '<div class="widget-right-list">';
+                    if($instance['your_checkbox_var1']){
+                        echo "<strong>Date:  </strong>";
+                        echo the_date('','','',TRUE);
+                    }
+                    if($instance['your_checkbox_var2']){
+                        echo "<br>";
+                        display_post_author_name();
+                    }
+                    if($instance['your_checkbox_var3']){
+                        echo "<br>";
+                        echo "<strong>Category:</strong>";
+                        echo "<br>";
+                        echo get_the_category_list();
+                    }
+                    echo '</div>';
+                    if($instance['your_checkbox_var4']){
+                        echo "<br>";
+                        echo coments_number();
+                    }
+                    if($instance['your_checkbox_var5']){
+                        echo "<br>";
+                        echo "<strong>Excerpt:</strong>";
+                        echo "<br>";
+                        the_excerpt();
+                    }
+                    echo "</li>";
+                    endwhile;
+                    
+                    echo '</div>';
+                    
+	}
+    }
+
+//add_action('widgets_init', create_function('', 'return register_widget("Recent_Posts");'));
+?>
+
+
---
+++ myplugin/recents.php
@@ -0,0 +1,141 @@
+<?php
+
+class Recent_Comments extends WP_Widget {
+
+	function __construct() {
+		$widget_ops = array('classname' => 'my_widget_class', 'description' => __('Insert the plugin description here'));
+	$control_ops = array('width' => 300, 'height' => 300);
+	parent::WP_Widget(false, $name = __('Recent_Comments'), $widget_ops, $control_ops );
+		$this->alt_option_name = 'widget_recent_comments';
+
+		if ( is_active_widget(false, false, $this->id_base) )
+			add_action( 'wp_head', array($this, 'recent_comments_style') );
+
+		add_action( 'comment_post', array($this, 'flush_widget_cache') );
+		add_action( 'edit_comment', array($this, 'flush_widget_cache') );
+		add_action( 'transition_comment_status', array($this, 'flush_widget_cache') );
+	}
+
+	function recent_comments_style() {
+		if ( ! current_theme_supports( 'widgets' ) // Temp hack #14876
+			|| ! apply_filters( 'show_recent_comments_widget_style', true, $this->id_base ) )
+			return;
+		?>
+	<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+<?php
+	}
+
+	/*function flush_widget_cache() {
+		wp_cache_delete('widget_recent_comments', 'widget');
+	}*/
+
+	function widget( $args, $instance ) {
+		global $comments, $comment;
+
+		echo '<div class="comments">';
+                 
+ 		extract($args, EXTR_SKIP);
+ 		$output = '';
+                
+		$title = ( ! empty( $instance['title'] ) ) ? $instance['title'] : __( 'Recent Comments' );
+                
+		$title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
+		$number = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : 5;
+		if ( ! $number )
+ 			$number = 5;
+
+		$comments = get_comments( apply_filters( 'widget_comments_args', array( 'number' => $number, 'status' => 'approve', 'post_status' => 'publish' ) ) );
+		$output .= $before_widget;
+		if ( $title )
+			$output .= $before_title . $title . $after_title;
+
+		$output .= '<ul id="recentcomments">';
+               
+                
+		if ( $comments ) {
+			// Prime cache for associated posts. (Prime post term cache if we need it for permalinks.)
+			$post_ids = array_unique( wp_list_pluck( $comments, 'comment_post_ID' ) );
+			_prime_post_caches( $post_ids, strpos( get_option( 'permalink_structure' ), '%category%' ), false );
+                       
+
+			foreach ( (array) $comments as $comment) {
+                            /*if($instance['checkbox_2']){
+                               
+                            $com=get_comment_text();}*/
+                             if($instance['checkbox_1']){
+                               
+                                // echo "on";
+                             $posttitle=get_the_title($comment->comment_post_ID);
+                             }
+                           echo '<div class="combine">';
+                             
+                             if($instance['checkbox_2']){
+                               echo '<div class="text">';
+                            $com=get_comment_text();
+                            echo '</div>';
+                             }
+                             
+                             if($instance['checkbox_3']){
+                                 $comm=get_comment_ID();
+                                 $id = get_comment($comm);
+                                 $image=get_avatar($id,'32');
+                                  
+                             }
+                            
+                           echo "</div>";
+                            $output .=  '<li class="recentcomments">' . /*translators: comments widget: 1: comment author, 2: post link*/  sprintf(_x('<br><br> %1$s on %2$s <br>  %3$s %4$s', 'widgets'), get_comment_author_link(), '<a href="' . esc_url( get_comment_link($comment->comment_ID) ) . '">' . $posttitle . '</a>' ,'<div class="combine"><div class="avatar">'.$image .'</div>','<div class="text">'.$com.'</div></div>') . '</li>';
+                                //$output .=  '<li class="recentcomments">' . /*translators: comments widget: 1: comment author, 2: post link*/  sprintf(_x('%1$s on %2$s <br> %3$s', 'widgets'), get_comment_author_link(), '<a href="' . esc_url( get_comment_link($comment->comment_ID) ) . '">' . get_the_title($comment->comment_post_ID) . '</a>',get_comment_text()) . '</li>';
+			}
+ 		}
+		//$output .= '</ul>';
+		$output .= $after_widget;
+
+		echo $output;
+		$cache[$args['widget_id']] = $output;
+		wp_cache_set('widget_recent_comments', $cache, 'widget');
+	}
+        
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['number'] = absint( $new_instance['number'] );
+		//$this->flush_widget_cache();
+
+		$alloptions = wp_cache_get( 'alloptions', 'options' );
+		if ( isset($alloptions['widget_recent_comments']) )
+			delete_option('widget_recent_comments');
+                
+                 $instance['checkbox_1']=strip_tags($new_instance['checkbox_1']);
+                  $instance['checkbox_2']=strip_tags($new_instance['checkbox_2']);
+                  $instance['checkbox_3']=strip_tags($new_instance['checkbox_3']);
+
+		return $instance;
+	}
+
+	function form( $instance ) {
+		$title  = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+		$number = isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
+?>
+		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></p>
+
+		
+                <input class="checkbox" type="checkbox" <?php checked($instance['checkbox_1'], 'on'); ?> id="<?php echo $this->get_field_id('checkbox_1'); ?>" name="<?php echo $this->get_field_name('checkbox_1'); ?>" /> 
+                <label for="<?php echo $this->get_field_id('checkbox_1'); ?>">Display post-title</label>
+        </p>
+        
+        <p>
+                <input class="checkbox" type="checkbox" <?php checked($instance['checkbox_2'], 'on'); ?> id="<?php echo $this->get_field_id('checkbox_2'); ?>" name="<?php echo $this->get_field_name('checkbox_2'); ?>" /> 
+                <label for="<?php echo $this->get_field_id('checkbox_2'); ?>">Display comment-text</label>
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of comments to show:' ); ?></label>
+            <input id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
+        <p>
+         <p>
+                <input class="checkbox" type="checkbox" <?php checked($instance['checkbox_3'], 'on'); ?> id="<?php echo $this->get_field_id('checkbox_3'); ?>" name="<?php echo $this->get_field_name('checkbox_3'); ?>" /> 
+                <label for="<?php echo $this->get_field_id('checkbox_3'); ?>">Display commenter's gravatar</label>
+        </p>
+       <?php 
+	}
+}
